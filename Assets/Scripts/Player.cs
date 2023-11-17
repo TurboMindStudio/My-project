f15e38baeccbb112af690c6426e9467b115c7cfc
TurboMindStudio <shivamchoudhari07@gmail.com>
2023-11-17 17:02:34 +0530

    playermove

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float jumpForce;
+    private Rigidbody rb;
+    public bool isGrounded;
+    public Transform groundPos;
+    public LayerMask layerName;
+    public float maxRange;
+    RaycastHit hit;
+    Ray ray;
+
+    private void Start()
     {
-        
+        rb=GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        raycastDetection();
     }
 
-    // Update is called once per frame
-    void Update()
+    void raycastDetection()
     {
-        
+        ray = new Ray(groundPos.position, -Vector3.up);
+        Debug.DrawRay(groundPos.position, -Vector3.up, Color.yellow);
+        if (Physics.Raycast(ray, out hit,maxRange,layerName))
+        {
+           
+            isGrounded=true;
+            Debug.Log("ground");
+
+            if(Input.GetMouseButtonDown(0)&&isGrounded)
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            isGrounded=false;
+        }
     }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce * Time.deltaTime, rb.velocity.z);
+    }
+
+
 }
